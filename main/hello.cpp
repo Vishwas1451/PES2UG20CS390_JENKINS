e2d95a4c6e7f59a034de4697c8084e9610462655
vishwas1451 <vishwas1451@gmail.com>
2023-02-15 00:13:20 +0530

    second commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, Everyone\n";
+  cout << "I am Groot\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
