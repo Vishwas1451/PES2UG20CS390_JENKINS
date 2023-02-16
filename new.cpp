048295d2bed7734d31377a5665a93edc0aa13853
vishwas1451 <vishwas1451@gmail.com>
2023-02-16 19:45:04 +0530

    Finally added this file

---
+++ new.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello,PES walooo \n";
+  cout << "Mai hoon DON \n";
+  cout << "I have successfully completed CC lab :) \n";
+  return 0;
+}
+
